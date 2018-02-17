5cd83eb6d6705733dc5d9d1c0f7e3471f82645ab
Israel777 <se.israel.abebe@gmail.com>
2018-02-17 16:18:36 +0300

    2d

--- pre.py
+++ pre.py
@@ -8,27 +8,27 @@ def convert_graysale(f):
     :return: returns the grayscaled and scaled version of the image
     '''
     image = cv2.imread(f,0)
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite(f, gray_image)
-    return gray_image
+    result = cv2.imwrite(f, image)
+    return result
 
 
-def grayscale_save(f):
+def grayscale_save(fi):
     '''
     :param f: file path
     :return: grayscales all images in the directory
     '''
     count = 0
-    for cur, _dirs, files in os.walk(f):
+    for cur, _dirs, files in os.walk(fi):
         head, tail = os.path.split(cur)
         while head:
             head, _tail = os.path.split(head)
 
         for f in files:
             if ".jpg" in f:
-                path =  tail + "/" + f
-                print(count , f ,path)
+                path =  fi + '/'+tail + "/" + f
+                a = convert_graysale(path)
+                print(count , f ,a)
                 count += 1
 
 
-grayscale_save()
+grayscale_save('data/images/train')
